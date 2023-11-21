00b0c1e354e032083491ff017124c91e64f33c79
Marcos <mail@novack.com.ar>
2023-11-21 19:08:28 -0300

    Single instance app
    This helps retrieve and show the current running instance, instead of launching multiple times the application.

---
+++ skiffWindowsApp/Skiff Desktop/WpfSingleInstance.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+
+namespace Skiff_Desktop
+{
+    /// <summary>
+    /// https://github.com/it3xl/WPF-app-Single-Instance-in-one-line-of-code
+    /// </summary>
+    public static class WpfSingleInstance
+    {
+        private static bool AlreadyProcessedOnThisInstance;
+
+
+        internal static void Make(string appName, bool uniquePerUser = true)
+        {
+            if (AlreadyProcessedOnThisInstance)
+            {
+                return;
+            }
+            AlreadyProcessedOnThisInstance = true;
+
+            Application app = Application.Current;
+
+            string eventName = uniquePerUser
+                ? $"{appName}-{Environment.MachineName}-{Environment.UserDomainName}-{Environment.UserName}"
+                : $"{appName}-{Environment.MachineName}";
+
+            bool isSecondaryInstance = true;
+
+            EventWaitHandle eventWaitHandle = null;
+            try
+            {
+                eventWaitHandle = EventWaitHandle.OpenExisting(eventName);
+            }
+            catch
+            {
+                // This code only runs on the first instance.
+                isSecondaryInstance = false;
+            }
+
+            if (isSecondaryInstance)
+            {
+                ActivateFirstInstanceWindow(eventWaitHandle);
+
+                // Let's produce a non-interceptable exit (2009 year approach).
+                Environment.Exit(0);
+            }
+
+            RegisterFirstInstanceWindowActivation(app, eventName);
+        }
+
+        private static void ActivateFirstInstanceWindow(EventWaitHandle eventWaitHandle)
+        {
+            // Let's notify the first instance to activate its main window.
+            _ = eventWaitHandle.Set();
+        }
+
+        private static void RegisterFirstInstanceWindowActivation(Application app, string eventName)
+        {
+            EventWaitHandle eventWaitHandle = new EventWaitHandle(
+                false,
+                EventResetMode.AutoReset,
+                eventName);
+
+            _ = ThreadPool.RegisterWaitForSingleObject(eventWaitHandle, WaitOrTimerCallback, app, Timeout.Infinite, false);
+
+            eventWaitHandle.Close();
+        }
+
+        private static void WaitOrTimerCallback(object state, bool timedOut)
+        {
+            Application app = (Application)state;
+            _ = app.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                //_ = Application.Current.MainWindow.Activate();
+                var skiffMainWindow = Application.Current.MainWindow as MainWindow;
+                skiffMainWindow?.OpenWindow();
+            }));
+        }
+    }
+}
